9715fd513bd66cce232748a508cff80dbd40f2db
Humma Noshine <Hummanoshine@gmail.com>
2021-12-14 20:29:58 +0000

    new tasks

---
+++ week3/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -0,0 +1,18 @@
+// Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = true;
+var expression2 = true;
+
+// Write Your JavaScript Code Here
+
+if (expression1 && expression2) {
+  console.log("True ✅ True ✅");
+}
+
+if (expression1) {
+  console.log("true x false x");
+}
+
+if (!expression1 & !expression2) {
+  console.log("false xx false");
+}
