034e31a3ce11a00762eb2a6e36628d2727569b1b
Humma Noshine <Hummanoshine@gmail.com>
2021-12-16 21:48:23 +0000

    more lessons

---
+++ week3/sess 2/Loops/Assets/index.js
@@ -0,0 +1,3 @@
+for (let number = 0; number < 1000; number = number + 1) {
+  console.log(number);
+}
---
+++ week3/sess 2/Loops/Task 2 loops/index.js
@@ -0,0 +1,7 @@
+let someNumbers = [];
+
+for (let i = 0; i < 1000; i++) {
+  someNumbers.push(i);
+}
+
+// New array where elements are the square of the elements in someNumbers
